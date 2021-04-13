c05bac127f5fd2efd3e66a45c88a4374979732ca
james-strom <james.stromnes@gmail.com>
2021-04-13 14:21:08 +0200

    first commit

---
+++ classexamples/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import React, {Component} from 'react';
+
+// function App() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );  
+//   return (
+//     <h3>{element}</h3>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
