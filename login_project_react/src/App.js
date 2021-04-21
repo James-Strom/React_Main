a049074fb047e07b85c163043078e77854f098bc
james-strom <james.stromnes@gmail.com>
2021-04-21 15:22:16 +0200

    fifth commit

--- login_project_react/src/App.js
+++ login_project_react/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import Products from './Products';
+// import Products from './Products';
 // import { Button } from 'react-bootstrap';
 // import Rating from './Rating';
-import JumboTronComponent from './JumboTronComponent';
+// import JumboTronComponent from './JumboTronComponent';
 import UserForm from './UserForm';
+import GitHub from './GitHub';
 
 class App extends Component {
   render() {
     const isValid = true;
     return (
       <div>
-        <JumboTronComponent />
-        <UserForm /> 
-        <Products />
+        <GitHub />
+        {/* <JumboTronComponent /> */}
+        {/* <UserForm />  */}
+        {/* <Products /> */}
           {/* <Button variant="primary" disabled={!isValid}>Default</Button> */}
          {/* <Rating rating="1"/>
          <Rating rating="2"/>
